d87fd75ca09c9b883752c8aec3757f321fcc58eb
Julie Delmas <julie.delmas33@gmail.com>
2018-09-12 13:43:24 +0200

    AddAdding new pages for website.

---
+++ footer.php
@@ -0,0 +1,15 @@
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 col-sm-6">
+
+            </div>
+        </div>
+
+        <div class="row">
+
+        </div>
+    </div>
+</footer>
+</body>
+</html>
