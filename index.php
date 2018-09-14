25179f322f6e677121073543ae1f529e4e5ebf05
Julie Delmas <julie.delmas33@gmail.com>
2018-09-14 13:48:26 +0200

    Deleting a book section (duplicate section book).

--- index.php
+++ index.php
@@ -36,52 +36,29 @@
 </section>
 
 <section id="book">
-    <div class="row justify-content-center py-5">
-        <form class="comment_livre_or" method="post" action="treatment.php">
-            <label for="comment">Please leave us your comment: </label><br />
-            <textarea name="comment" id="comment" placeholder="Your comment here" rows="8" cols="40"></textarea>
-            <br>
-            <input type="button" value="Send" class="button"/>
-        </form>
-    </div>
+    <div class="container">
+        <div class="row justify-content-center py-5">
+            <form class="comment_livre_or" method="post" action="treatment.php">
+                <label for="comment">Please leave us your comment: </label><br />
+                <textarea name="comment" id="comment" placeholder="Your comment here" rows="8" cols="40"></textarea>
+                <br>
+                <input type="button" value="Send" class="button"/>
+            </form>
+        </div>
 
-    <div class="row justify-content-around">
-        <div class="col-lg-5 col-md-4 col-sm-10 text-justify py-2">
-            <p class="small"><span class="h1">"</span>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus.<span class="h1">"</span></p>
-        </div>
-        <div class="col-lg-5 col-md-4 col-sm-10 text-justify py-2">
-            <p class="small"><span class="h1">"</span>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus.<span class="h1">"</span></p>
-        </div>
-        <div class="col-lg-5 col-md-4 col-sm-10 text-justify py-2">
-            <p class="small"><span class="h1">"</span>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus.<span class="h1">"</span></p>
-        </div>
-        <div class="col-lg-5 col-md-4 col-sm-10 text-justify py-2">
-            <p class="small"><span class="h1">"</span>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus.<span class="h1">"</span></p>
-        </div>
-    </div>
-  
-<div class="row justify-content-center py-5">
-                <form class="comment_livre_or" method="post" action="treatment.php">
-                    <label for="comment">Please leave us your comment: </label><br />
-                    <textarea name="comment" id="comment" placeholder="Your comment here" rows="8" cols="40"></textarea>
-                        
-                    <br />
-                    <input type="button" value="Send" class="button"/>
-                </form>
-    </div>
-
-    <div class="row justify-content-around">
-        <div class="col-lg-5 col-md-4 col-sm-10 text-justify py-2">
-            <p class="small"><span class="h1">"</span>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus.<span class="h1">"</span></p>
-        </div>
-        <div class="col-lg-5 col-md-4 col-sm-10 text-justify py-2">
-            <p class="small"><span class="h1">"</span>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus.<span class="h1">"</span></p>
-        </div>
-        <div class="col-lg-5 col-md-4 col-sm-10 text-justify py-2">
-            <p class="small"><span class="h1">"</span>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus.<span class="h1">"</span></p>
-        </div>
-        <div class="col-lg-5 col-md-4 col-sm-10 text-justify py-2">
-            <p class="small"><span class="h1">"</span>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus.<span class="h1">"</span></p>
+        <div class="row justify-content-around">
+            <div class="col-lg-5 col-md-4 col-sm-10 text-justify py-2">
+                <p class="small"><span class="h1">"</span>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus.<span class="h1">"</span></p>
+            </div>
+            <div class="col-lg-5 col-md-4 col-sm-10 text-justify py-2">
+                <p class="small"><span class="h1">"</span>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus.<span class="h1">"</span></p>
+            </div>
+            <div class="col-lg-5 col-md-4 col-sm-10 text-justify py-2">
+                <p class="small"><span class="h1">"</span>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus.<span class="h1">"</span></p>
+            </div>
+            <div class="col-lg-5 col-md-4 col-sm-10 text-justify py-2">
+                <p class="small"><span class="h1">"</span>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus.<span class="h1">"</span></p>
+            </div>
         </div>
     </div>
 </section>
@@ -93,7 +70,7 @@
         </div>
         <div class="row">
             <div class="col">
-            <?php require ("contact.php"); ?>
+                <?php require ("contact.php"); ?>
             </div>
         </div>
     </div>
