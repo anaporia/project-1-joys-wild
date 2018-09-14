62623fb5430cb7a792cbe9ff8ea977375e0c9176
Julie Delmas <julie.delmas33@gmail.com>
2018-09-14 13:31:50 +0200

    Corrections on sections (suppr container and adding div class container).

--- footer.php
+++ footer.php
@@ -1,32 +1,26 @@
 <footer class="page-footer font-small orange">
-    <section class="container">
-        <div class="row text-center d-flex justify-content-center pt-5 mb-3">
-            <div class="col-xs-12 col-sm-3">
-                <h6 class="text-uppercase font-weight-bold">
+    <div class="container">
+        <ul class="row text-center d-flex justify-content-center pt-5 mb-3">
+            <li class="col-xs-12 col-sm-2 text-uppercase font-weight-bold">
                     <a href="faq.php">F.A.Q.</a>
-                </h6>
-            </div>
-            <div class="col-xs-12 col-sm-3">
-                <h6 class="text-uppercase font-weight-bold">
+            </li>
+            <li class="col-xs-12 col-sm-3 text-uppercase font-weight-bold">
                     <a href="mention.php">Mentions légales</a>
-                </h6>
-            </div>
-            <div class="col-xs-12 col-sm-2">
-                <h6 class="text-uppercase font-weight-bold">
+            </li>
+            <li class="col-xs-12 col-sm-3 text-uppercase font-weight-bold">
                     <a href="index.php#contact">Contact</a>
-                </h6>
-            </div>
-            <div class="col-xs-12 col"><!-- Facebook -->
-                <img class="high" src="img/facebook.png" alt="facebook"></img>
-            </div>
-            <div class="col">
-                <img class="high" src="img/twitter.png" alt="Twitter"></img>
-            </div>
-        </div>
+            </li>
+            <li class="col-xs-12 col-sm-2 text-uppercase font-weight-bold">
+                <img class="high" src="img/facebook.png" alt="facebook">
+            </li>
+            <li class="col-xs-12 col-sm-2 text-uppercase font-weight-bold">
+                <img class="high" src="img/twitter.png" alt="Twitter">
+            </li>
+        </ul>
         <div class="row">
             <div class="text-center py-3">
                 <a href="https://wildcodeschool.fr/" target="_blank">Made with love by Wild Code School</a>
             </div>
         </div>
-    </section>
+    </div>
 </footer>
