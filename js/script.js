c5cb1d61236bdd6a3d7f8ab9f9f8ef7d467d114a
Julie Delmas <julie.delmas33@gmail.com>
2018-09-18 23:40:56 +0200

    Adding function ajax for contact-form.

---
+++ js/script.js
@@ -0,0 +1,29 @@
+$(function () {
+
+    $('#contact-form').submit(function(e) {
+        e.preventDefault();
+        $('.comments').empty();
+        var postdata = $('#contact-form').serialize();
+
+        $.ajax({
+            type: 'POST',
+            url: 'php/contact.php',
+            data: postdata,
+            dataType: 'json',
+            success: function(json) {
+
+                if(json.isSuccess) {
+                    $('#contact-form').append("<p class='thank-you'>Data !!!! Even more data !!!! Full of data !!!!! Mouhaha !!!</p>");
+                    $('#contact-form')[0].reset();
+                } else {
+                    $('#firstname + .comments').html(json.firstnameError);
+                    $('#name + .comments').html(json.nameError);
+                    $('#email + .comments').html(json.emailError);
+                    $('#phone + .comments').html(json.phoneError);
+                    $('#subject + .comments').html(json.subjectError);
+                    $('#message + .comments').html(json.messageError);
+                }
+            }
+        });
+    });
+})
