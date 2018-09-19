9bc5b1ec9edeb647919ad4d2a8adc1b7ff72bfa3
Arlensiu Celis <arlensiu.celis@gmail.com>
2018-09-19 09:38:48 +0200

    Change in the CSS page that we call, now it is style.css instead of template.css.

--- trainers.php
+++ trainers.php
@@ -7,7 +7,7 @@
   	<meta name="keywords" content="HTML,CSS,PHP,JavaScript, Wild Code School">
   	<meta name="authors" content="Nicolas Duhamel, Emilie Boeglen, Julie Dulmas, Steven Antal, Arlensiú Celis">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/template.css">
+    <link rel="stylesheet" href="css/style.css">
     <title>WCS - Trainers</title>
 </head>
 
