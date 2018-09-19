a759f109c949f993acc69ca4315ab14aae51fc81
Arlensiu Celis <arlensiu.celis@gmail.com>
2018-09-19 15:31:16 +0200

    Semi final version guestbook.

--- guestbooktrial.js
+++ guestbooktrial.js
@@ -1,60 +1,65 @@
 let guestbookEntries = [                                // 1 - We create a set of fake data to begin with //
     {
         Message: "This site is full of super funny !! I'd visit it again and again just for the gifs xDD",
-        Date: "15 September, 2018 12:50"
+        Date: "Sep 15, 2018 12:50"
     },
     {
         Message: "Truth to be told, my budget on socks has increased considerably since I arrived to the Wild School",
-        Date: "17 September, 2018 03:30"
+        Date: "Sep 17, 2018 03:30"
     },
     {
         Message: "For the moment we do not have CSS but it will come !!",
-        Date: "18 September, 2018 07:45"
+        Date: "Sep 18, 2018 07:45"
     },
     {
         Message: "I am most certainly in the future, as we already had a few beers and well ... the date is weird ...",
-        Date: "19 September, 2018 18:50"
+        Date: "Sep 19, 2018 18:50"
     }
 ];
 
-function postGuestbook() {                                  // 12 - make the appearance of the guestbook as a function to later use it
-    const guestbook = document.querySelector("#book");      // 2 - this tells us which place we are going to put stuff into, in this case is the UL with ID book
-    guestbook.innerHTML = null;                             // 15 - We realize the list must be empty before we print it
+function postGuestbook() {                                  			// 12 - make the appearance of the guestbook as a function to later use it
+    const guestbook = document.querySelector("#book");      			// 2 - this tells us which place we are going to put stuff into, in this case is the UL with ID book
+    guestbook.innerHTML = null;                             			// 15 - We realize the list must be empty before we print it
 
-    for (let currentEntry of guestbookEntries){             // 3 - we will first create a list that displays all the previously stored messages //
-        let postMessage = document.createElement("li");      // 4 - here we create a new html element with a newMessage variable, in this case a li //
-        let postDate = document.createElement("li");
+    for (let currentEntry of guestbookEntries){             			// 3 - we will first create a list that displays all the previously stored messages //
+        let postImage = document.createElement("div");
+        let postMessage = document.createElement("div");      			// 4 - here we create a new html element with a newMessage variable, in this case a li //
+        let postDate = document.createElement("div");
 
-        postMessage.innerHTML = currentEntry.Message;        // 5 - this tells the class or id or whatever html element that inside itself there must be certain information //
+		postImage.classList.add("col-3imageGuest");
+		postMessage.classList.add("col-9messageGuest");
+		postDate.classList.add("col-3dateGuest");
+
+        postMessage.innerHTML = currentEntry.Message;       			// 5 - this tells the class or id or whatever html element that inside itself there must be certain information //
         postDate.innerHTML = currentEntry.Date;
-        guestbook.append(postMessage);                       // 6 - I am saying that on the ID that we identified before, we will now append the created LI
+
+        guestbook.append(postImage);
+        guestbook.append(postMessage);                       			// 6 - I am saying that on the ID that we identified before, we will now append the created LI
         guestbook.append(postDate);
     }
 };
 
 function writeDate(todayDate){
-	
-	function monthNumberToText(monthNumber){
-		const monthText = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-		return monthText[monthNumber];
-	}
-	return todayDate.getDate() + " " + monthNumberToText(todayDate.getMonth()) + ", " + todayDate.getFullYear() + " " + todayDate.getHours() + ":" + todayDate.getMinutes();
+	return todayDate.toString().slice(4, 21).replace(" 2", ", 2");		// 17 - Function that converts the date on a string in the format we want
+}
 
-}															// 17 - Function that writes the date in the right format as a string
+postGuestbook();                                            			// 14 - You realize there is no list to begin with so you put the list before any changes
 
-postGuestbook();                                            // 14 - You realize there is no list to begin with so you put the list before any changes
-
-let buttonGuestbook = document.querySelector("#buttonGuestbook");    // 7 - Tell to get the element button
+let buttonGuestbook = document.querySelector("#buttonGuestbook");    	// 7 - Tell to get the element button
     buttonGuestbook.addEventListener("click", function(){               // 8 - Add event click to the element button
 
-    let newMessage = prompt("Write a new message: ");               // 9 - Ask user the info, I have to change this to a get string from Text-area field
+//    let newMessage = prompt("Write a new message: ");               	// 9 - Ask user the info, I have to change this to a get string from Text-area field
+    let newMessage = document.querySelector("textarea").value;          // 9 - Info comes from the textarea box and .value gets the inside of the box
     let today = new Date();
 
-    let newEntry = {                                            // 10 - Create a new entry for the guestbookEntries with a message as the received info
+    let newEntry = {                                            		// 10 - Create a new entry for the guestbookEntries with a message as the received info
         Message: newMessage,
-        Date: writeDate(today)								// 16 - Define a new date for each entry that calls a function to write the proper date
+        Date: writeDate(today)											// 16 - Define a new date for each entry that calls a function to write the proper date
     };
 
-    guestbookEntries.push(newEntry);                            // 11 - Put the new entry as part of the Guestbook array
-    postGuestbook();                                            // 13 - Make the list appear once the data is asked and can be added to show
+    guestbookEntries.push(newEntry);                            		// 11 - Put the new entry as part of the Guestbook array
+    postGuestbook();                                            		// 13 - Make the list appear once the data is asked and can be added to show
+
+    document.querySelector("textarea").value = null;					// 18 - Empty the textarea box after the message has been sent
+
     });
--- indextrial.php
+++ indextrial.php
@@ -5,22 +5,44 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="css/templatetrial.css">
     <title>Document</title>
 </head>
 
 <body>
 
-    <ul id="book">
-        <!--Here is where the tasks go-->
-    </ul>
+	<div class="container">
+		<section id="guestbook">
+	      	<h2 class="text-center">Wild Guestbook</h2>
+	   		<div class="row">
+	   			<div class="col d-flex flex-column">
+			   		<form class="guestbook-request d-flex justify-content-center">
+							<p>
+						   		<label for="comment">Please leave us your comment: </label><br />
+			           			<textarea name="comment" id="comment" placeholder="Your comment here" rows="5" cols="60"></textarea>
+							
+								<br />
+							</p>
+					</form>
+					<button id="buttonGuestbook" class="align-self-center">Send</button>
+				</div>
+			</div>
 
-    <button id="buttonGuestbook">Send</button>
-
-
-    <!-- For the guestbook I just convert the ul in a row and the li in divs.-->
+			<div class="row my-5">
+				<div class="col" id="book">
+				    <!--Here is where the reviews go-->
+				</div>
+			</div>
+		</section>
+	</div>
 
     <script src="guestbooktrial.js"></script>
+
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
 
 </body>
 
