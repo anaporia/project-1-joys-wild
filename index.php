05a2155861d9bd5d6a946dc13a42a4866d1e6f24
Arlensiu Celis <arlensiu.celis@gmail.com>
2018-09-21 08:53:48 +0200

    Adding favicon to the index.

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <title>Wild Code School</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="shortcut icon" type="image/png" href="img/faviconwild.png"/>
 </head>
 
 <body>
